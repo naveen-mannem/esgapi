baf081a65e8056df8cb7eda24a317a11dff8c950
vivekk99 <vivekkumar1076@gmail.com>
2021-05-09 21:54:15 -0700

    Create model.js

---
+++ src/api/KMP/model.js
@@ -0,0 +1,51 @@
+import mongoose, { Schema } from 'mongoose'
+
+const kmpSchema = new Schema({
+  companyId: {
+    type: String
+  },
+  kmpMemberName: {
+    type: String
+  },
+  memberStatus: {
+    type: String
+  },
+  createdAt: {
+    type: String
+  },
+  updatedAt: {
+    type: String
+  }
+}, {
+  timestamps: true,
+  toJSON: {
+    virtuals: true,
+    transform: (obj, ret) => { delete ret._id }
+  }
+})
+
+kmpSchema.methods = {
+  view (full) {
+    const view = {
+      // simple view
+      id: this.id,
+      companyId: this.companyId,
+      kmpMemberName: this.kmpMemberName,
+      memberStatus: this.memberStatus,
+      createdAt: this.createdAt,
+      updatedAt: this.updatedAt,
+      createdAt: this.createdAt,
+      updatedAt: this.updatedAt
+    }
+
+    return full ? {
+      ...view
+      // add properties for a full view
+    } : view
+  }
+}
+
+const model = mongoose.model('Kmp', kmpSchema)
+
+export const schema = model.schema
+export default model
