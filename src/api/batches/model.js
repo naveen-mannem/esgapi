63b3a7d64a8778898605351cf580c7ac4c412ea8
vivekk99 <vivekkumar1076@gmail.com>
2021-05-10 22:10:31 -0700

    Create model.js

---
+++ src/api/batches/model.js
@@ -0,0 +1,50 @@
+import mongoose, { Schema } from 'mongoose'
+
+const batchesSchema = new Schema({
+  createdBy: {
+    type: Schema.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  batchName: {
+    type: String
+  },
+  batchSLA: {
+    type: String
+  },
+  status: {
+    type: Boolean,
+    default: true
+  }
+}, {
+  timestamps: true,
+  toJSON: {
+    virtuals: true,
+    transform: (obj, ret) => { delete ret._id }
+  }
+})
+
+batchesSchema.methods = {
+  view (full) {
+    const view = {
+      // simple view
+      id: this.id,
+      createdBy: this.createdBy ? this.createdBy.view(full) : null,
+      batchName: this.batchName,
+      batchSLA: this.batchSLA,
+      status: this.status,
+      createdAt: this.createdAt,
+      updatedAt: this.updatedAt
+    }
+
+    return full ? {
+      ...view
+      // add properties for a full view
+    } : view
+  }
+}
+
+const model = mongoose.model('Batches', batchesSchema)
+
+export const schema = model.schema
+export default model
