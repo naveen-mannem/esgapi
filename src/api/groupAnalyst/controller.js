f709011efc2ddecc446a239c32e47f60b003c403
vivekk99 <vivekkumar1076@gmail.com>
2021-05-10 22:16:11 -0700

    Update controller.js

--- src/api/groupAnalyst/controller.js
+++ src/api/groupAnalyst/controller.js
@@ -1,8 +1,8 @@
-import { success, notFound } from '../../services/response/'
+import { success, notFound, authorOrAdmin } from '../../services/response/'
 import { GroupAnalyst } from '.'
 
-export const create = ({ bodymen: { body } }, res, next) =>
-  GroupAnalyst.create(body)
+export const create = ({ user, bodymen: { body } }, res, next) =>
+  GroupAnalyst.create({ ...body, createdBy: user })
     .then((groupAnalyst) => groupAnalyst.view(true))
     .then(success(res, 201))
     .catch(next)
@@ -10,6 +10,9 @@ export const create = ({ bodymen: { body } }, res, next) =>
 export const index = ({ querymen: { query, select, cursor } }, res, next) =>
   GroupAnalyst.count(query)
     .then(count => GroupAnalyst.find(query, select, cursor)
+      .populate('createdBy')
+      .populate('userId')
+      .populate('groupId')
       .then((groupAnalysts) => ({
         count,
         rows: groupAnalysts.map((groupAnalyst) => groupAnalyst.view())
@@ -20,22 +23,30 @@ export const index = ({ querymen: { query, select, cursor } }, res, next) =>
 
 export const show = ({ params }, res, next) =>
   GroupAnalyst.findById(params.id)
+    .populate('createdBy')
+    .populate('userId')
+    .populate('groupId')
     .then(notFound(res))
     .then((groupAnalyst) => groupAnalyst ? groupAnalyst.view() : null)
     .then(success(res))
     .catch(next)
 
-export const update = ({ bodymen: { body }, params }, res, next) =>
+export const update = ({ user, bodymen: { body }, params }, res, next) =>
   GroupAnalyst.findById(params.id)
+    .populate('createdBy')
+    .populate('userId')
+    .populate('groupId')
     .then(notFound(res))
+    .then(authorOrAdmin(res, user, 'createdBy'))
     .then((groupAnalyst) => groupAnalyst ? Object.assign(groupAnalyst, body).save() : null)
     .then((groupAnalyst) => groupAnalyst ? groupAnalyst.view(true) : null)
     .then(success(res))
     .catch(next)
 
-export const destroy = ({ params }, res, next) =>
+export const destroy = ({ user, params }, res, next) =>
   GroupAnalyst.findById(params.id)
     .then(notFound(res))
+    .then(authorOrAdmin(res, user, 'createdBy'))
     .then((groupAnalyst) => groupAnalyst ? groupAnalyst.remove() : null)
     .then(success(res, 204))
     .catch(next)
