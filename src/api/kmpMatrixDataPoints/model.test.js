fba89f96d88718152b35abc890c66d968f1b4d9f
vivekk99 <vivekkumar1076@gmail.com>
2021-05-10 22:21:38 -0700

    Update model.test.js

--- src/api/kmpMatrixDataPoints/model.test.js
+++ src/api/kmpMatrixDataPoints/model.test.js
@@ -1,9 +1,11 @@
 import { KmpMatrixDataPoints } from '.'
+import { User } from '../user'
 
-let kmpMatrixDataPoints
+let user, kmpMatrixDataPoints
 
 beforeEach(async () => {
-  kmpMatrixDataPoints = await KmpMatrixDataPoints.create({ kmpId: 'test', dpCodeId: 'test', response: 'test', year: 'test', status: 'test', createdAt: 'test', updatedAt: 'test' })
+  user = await User.create({ email: 'a@a.com', password: '123456' })
+  kmpMatrixDataPoints = await KmpMatrixDataPoints.create({ createdBy: user, kmpId: 'test', dpCodeId: 'test', response: 'test', year: 'test', status: 'test' })
 })
 
 describe('view', () => {
@@ -11,13 +13,13 @@ describe('view', () => {
     const view = kmpMatrixDataPoints.view()
     expect(typeof view).toBe('object')
     expect(view.id).toBe(kmpMatrixDataPoints.id)
+    expect(typeof view.createdBy).toBe('object')
+    expect(view.createdBy.id).toBe(user.id)
     expect(view.kmpId).toBe(kmpMatrixDataPoints.kmpId)
     expect(view.dpCodeId).toBe(kmpMatrixDataPoints.dpCodeId)
     expect(view.response).toBe(kmpMatrixDataPoints.response)
     expect(view.year).toBe(kmpMatrixDataPoints.year)
     expect(view.status).toBe(kmpMatrixDataPoints.status)
-    expect(view.createdAt).toBe(kmpMatrixDataPoints.createdAt)
-    expect(view.updatedAt).toBe(kmpMatrixDataPoints.updatedAt)
     expect(view.createdAt).toBeTruthy()
     expect(view.updatedAt).toBeTruthy()
   })
@@ -26,13 +28,13 @@ describe('view', () => {
     const view = kmpMatrixDataPoints.view(true)
     expect(typeof view).toBe('object')
     expect(view.id).toBe(kmpMatrixDataPoints.id)
+    expect(typeof view.createdBy).toBe('object')
+    expect(view.createdBy.id).toBe(user.id)
     expect(view.kmpId).toBe(kmpMatrixDataPoints.kmpId)
     expect(view.dpCodeId).toBe(kmpMatrixDataPoints.dpCodeId)
     expect(view.response).toBe(kmpMatrixDataPoints.response)
     expect(view.year).toBe(kmpMatrixDataPoints.year)
     expect(view.status).toBe(kmpMatrixDataPoints.status)
-    expect(view.createdAt).toBe(kmpMatrixDataPoints.createdAt)
-    expect(view.updatedAt).toBe(kmpMatrixDataPoints.updatedAt)
     expect(view.createdAt).toBeTruthy()
     expect(view.updatedAt).toBeTruthy()
   })
