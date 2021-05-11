3fddf8db7f839ad80baafdacd884310b7062efa1
vivekk99 <vivekkumar1076@gmail.com>
2021-05-10 22:14:23 -0700

    Update model.test.js

--- src/api/errorDetails/model.test.js
+++ src/api/errorDetails/model.test.js
@@ -1,9 +1,11 @@
 import { ErrorDetails } from '.'
+import { User } from '../user'
 
-let errorDetails
+let user, errorDetails
 
 beforeEach(async () => {
-  errorDetails = await ErrorDetails.create({ errorTypeId: 'test', taskId: 'test', loggedBy: 'test', comments: 'test', errorLoggedDate: 'test', errorStatus: 'test', standAlonId: 'test', status: 'test', createdAt: 'test', updatedAt: 'test' })
+  user = await User.create({ email: 'a@a.com', password: '123456' })
+  errorDetails = await ErrorDetails.create({ createdBy: user, errorTypeId: 'test', taskId: 'test', loggedBy: 'test', comments: 'test', errorLoggedDate: 'test', errorStatus: 'test', standaloneId: 'test', status: 'test' })
 })
 
 describe('view', () => {
@@ -11,16 +13,16 @@ describe('view', () => {
     const view = errorDetails.view()
     expect(typeof view).toBe('object')
     expect(view.id).toBe(errorDetails.id)
+    expect(typeof view.createdBy).toBe('object')
+    expect(view.createdBy.id).toBe(user.id)
     expect(view.errorTypeId).toBe(errorDetails.errorTypeId)
     expect(view.taskId).toBe(errorDetails.taskId)
     expect(view.loggedBy).toBe(errorDetails.loggedBy)
     expect(view.comments).toBe(errorDetails.comments)
     expect(view.errorLoggedDate).toBe(errorDetails.errorLoggedDate)
     expect(view.errorStatus).toBe(errorDetails.errorStatus)
-    expect(view.standAlonId).toBe(errorDetails.standAlonId)
+    expect(view.standaloneId).toBe(errorDetails.standaloneId)
     expect(view.status).toBe(errorDetails.status)
-    expect(view.createdAt).toBe(errorDetails.createdAt)
-    expect(view.updatedAt).toBe(errorDetails.updatedAt)
     expect(view.createdAt).toBeTruthy()
     expect(view.updatedAt).toBeTruthy()
   })
@@ -29,16 +31,16 @@ describe('view', () => {
     const view = errorDetails.view(true)
     expect(typeof view).toBe('object')
     expect(view.id).toBe(errorDetails.id)
+    expect(typeof view.createdBy).toBe('object')
+    expect(view.createdBy.id).toBe(user.id)
     expect(view.errorTypeId).toBe(errorDetails.errorTypeId)
     expect(view.taskId).toBe(errorDetails.taskId)
     expect(view.loggedBy).toBe(errorDetails.loggedBy)
     expect(view.comments).toBe(errorDetails.comments)
     expect(view.errorLoggedDate).toBe(errorDetails.errorLoggedDate)
     expect(view.errorStatus).toBe(errorDetails.errorStatus)
-    expect(view.standAlonId).toBe(errorDetails.standAlonId)
+    expect(view.standaloneId).toBe(errorDetails.standaloneId)
     expect(view.status).toBe(errorDetails.status)
-    expect(view.createdAt).toBe(errorDetails.createdAt)
-    expect(view.updatedAt).toBe(errorDetails.updatedAt)
     expect(view.createdAt).toBeTruthy()
     expect(view.updatedAt).toBeTruthy()
   })
