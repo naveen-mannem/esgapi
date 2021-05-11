ef6ff934ccb715ce854e220e03e90fcd3b2bd1b3
vivekk99 <vivekkumar1076@gmail.com>
2021-05-10 22:16:41 -0700

    Update model.test.js

--- src/api/groupAnalyst/model.test.js
+++ src/api/groupAnalyst/model.test.js
@@ -1,9 +1,11 @@
 import { GroupAnalyst } from '.'
+import { User } from '../user'
 
-let groupAnalyst
+let user, groupAnalyst
 
 beforeEach(async () => {
-  groupAnalyst = await GroupAnalyst.create({ userId: 'test', groupId: 'test', status: 'test', createdAt: 'test', updatedAt: 'test' })
+  user = await User.create({ email: 'a@a.com', password: '123456' })
+  groupAnalyst = await GroupAnalyst.create({ createdBy: user, userId: 'test', groupId: 'test', status: 'test' })
 })
 
 describe('view', () => {
@@ -11,11 +13,11 @@ describe('view', () => {
     const view = groupAnalyst.view()
     expect(typeof view).toBe('object')
     expect(view.id).toBe(groupAnalyst.id)
+    expect(typeof view.createdBy).toBe('object')
+    expect(view.createdBy.id).toBe(user.id)
     expect(view.userId).toBe(groupAnalyst.userId)
     expect(view.groupId).toBe(groupAnalyst.groupId)
     expect(view.status).toBe(groupAnalyst.status)
-    expect(view.createdAt).toBe(groupAnalyst.createdAt)
-    expect(view.updatedAt).toBe(groupAnalyst.updatedAt)
     expect(view.createdAt).toBeTruthy()
     expect(view.updatedAt).toBeTruthy()
   })
@@ -24,11 +26,11 @@ describe('view', () => {
     const view = groupAnalyst.view(true)
     expect(typeof view).toBe('object')
     expect(view.id).toBe(groupAnalyst.id)
+    expect(typeof view.createdBy).toBe('object')
+    expect(view.createdBy.id).toBe(user.id)
     expect(view.userId).toBe(groupAnalyst.userId)
     expect(view.groupId).toBe(groupAnalyst.groupId)
     expect(view.status).toBe(groupAnalyst.status)
-    expect(view.createdAt).toBe(groupAnalyst.createdAt)
-    expect(view.updatedAt).toBe(groupAnalyst.updatedAt)
     expect(view.createdAt).toBeTruthy()
     expect(view.updatedAt).toBeTruthy()
   })
