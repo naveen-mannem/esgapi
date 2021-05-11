563ec0ddf7ddf2cbd763e77bfe0cfffd22679a45
vivekk99 <vivekkumar1076@gmail.com>
2021-05-10 22:22:44 -0700

    Update controller.js

--- src/api/taskAssignment/controller.js
+++ src/api/taskAssignment/controller.js
@@ -1,8 +1,8 @@
-import { success, notFound } from '../../services/response/'
+import { success, notFound, authorOrAdmin } from '../../services/response/'
 import { TaskAssignment } from '.'
 
-export const create = ({ bodymen: { body } }, res, next) =>
-  TaskAssignment.create(body)
+export const create = ({ user, bodymen: { body } }, res, next) =>
+  TaskAssignment.create({ ...body, createdBy: user })
     .then((taskAssignment) => taskAssignment.view(true))
     .then(success(res, 201))
     .catch(next)
@@ -10,6 +10,12 @@ export const create = ({ bodymen: { body } }, res, next) =>
 export const index = ({ querymen: { query, select, cursor } }, res, next) =>
   TaskAssignment.count(query)
     .then(count => TaskAssignment.find(query, select, cursor)
+      .populate('createdBy')
+      .populate('companyId')
+      .populate('categoryId')
+      .populate('groupId')
+      .populate('analystId')
+      .populate('qaId')
       .then((taskAssignments) => ({
         count,
         rows: taskAssignments.map((taskAssignment) => taskAssignment.view())
@@ -20,22 +26,36 @@ export const index = ({ querymen: { query, select, cursor } }, res, next) =>
 
 export const show = ({ params }, res, next) =>
   TaskAssignment.findById(params.id)
+      .populate('createdBy')
+      .populate('companyId')
+      .populate('categoryId')
+      .populate('groupId')
+      .populate('analystId')
+      .populate('qaId')
     .then(notFound(res))
     .then((taskAssignment) => taskAssignment ? taskAssignment.view() : null)
     .then(success(res))
     .catch(next)
 
-export const update = ({ bodymen: { body }, params }, res, next) =>
+export const update = ({ user, bodymen: { body }, params }, res, next) =>
   TaskAssignment.findById(params.id)
+      .populate('createdBy')
+      .populate('companyId')
+      .populate('categoryId')
+      .populate('groupId')
+      .populate('analystId')
+      .populate('qaId')
     .then(notFound(res))
+    .then(authorOrAdmin(res, user, 'createdBy'))
     .then((taskAssignment) => taskAssignment ? Object.assign(taskAssignment, body).save() : null)
     .then((taskAssignment) => taskAssignment ? taskAssignment.view(true) : null)
     .then(success(res))
     .catch(next)
 
-export const destroy = ({ params }, res, next) =>
+export const destroy = ({ user, params }, res, next) =>
   TaskAssignment.findById(params.id)
     .then(notFound(res))
+    .then(authorOrAdmin(res, user, 'createdBy'))
     .then((taskAssignment) => taskAssignment ? taskAssignment.remove() : null)
     .then(success(res, 204))
     .catch(next)
