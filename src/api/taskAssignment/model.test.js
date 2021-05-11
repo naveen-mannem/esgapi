e3a5f4431b3754305b106dcfbbb2e15887b06eb2
vivekk99 <vivekkumar1076@gmail.com>
2021-05-10 22:23:09 -0700

    Update model.test.js

--- src/api/taskAssignment/model.test.js
+++ src/api/taskAssignment/model.test.js
@@ -1,9 +1,11 @@
 import { TaskAssignment } from '.'
+import { User } from '../user'
 
-let taskAssignment
+let user, taskAssignment
 
 beforeEach(async () => {
-  taskAssignment = await TaskAssignment.create({ companyId: 'test', categoryId: 'test', groupId: 'test', revisionCode: 'test', assignedTo: 'test', year: 'test', analystSLA: 'test', taskStatus: 'test', analystEmail: 'test', qaEmail: 'test', qaSLA: 'test', status: 'test', createdAt: 'test', updatedAt: 'test' })
+  user = await User.create({ email: 'a@a.com', password: '123456' })
+  taskAssignment = await TaskAssignment.create({ createdBy: user, companyId: 'test', categoryId: 'test', groupId: 'test', revisionCode: 'test', assignedTo: 'test', year: 'test', analystSLA: 'test', taskStatus: 'test', analystId: 'test', qaId: 'test', status: 'test' })
 })
 
 describe('view', () => {
@@ -11,6 +13,8 @@ describe('view', () => {
     const view = taskAssignment.view()
     expect(typeof view).toBe('object')
     expect(view.id).toBe(taskAssignment.id)
+    expect(typeof view.createdBy).toBe('object')
+    expect(view.createdBy.id).toBe(user.id)
     expect(view.companyId).toBe(taskAssignment.companyId)
     expect(view.categoryId).toBe(taskAssignment.categoryId)
     expect(view.groupId).toBe(taskAssignment.groupId)
@@ -19,12 +23,9 @@ describe('view', () => {
     expect(view.year).toBe(taskAssignment.year)
     expect(view.analystSLA).toBe(taskAssignment.analystSLA)
     expect(view.taskStatus).toBe(taskAssignment.taskStatus)
-    expect(view.analystEmail).toBe(taskAssignment.analystEmail)
-    expect(view.qaEmail).toBe(taskAssignment.qaEmail)
-    expect(view.qaSLA).toBe(taskAssignment.qaSLA)
+    expect(view.analystId).toBe(taskAssignment.analystId)
+    expect(view.qaId).toBe(taskAssignment.qaId)
     expect(view.status).toBe(taskAssignment.status)
-    expect(view.createdAt).toBe(taskAssignment.createdAt)
-    expect(view.updatedAt).toBe(taskAssignment.updatedAt)
     expect(view.createdAt).toBeTruthy()
     expect(view.updatedAt).toBeTruthy()
   })
@@ -33,6 +34,8 @@ describe('view', () => {
     const view = taskAssignment.view(true)
     expect(typeof view).toBe('object')
     expect(view.id).toBe(taskAssignment.id)
+    expect(typeof view.createdBy).toBe('object')
+    expect(view.createdBy.id).toBe(user.id)
     expect(view.companyId).toBe(taskAssignment.companyId)
     expect(view.categoryId).toBe(taskAssignment.categoryId)
     expect(view.groupId).toBe(taskAssignment.groupId)
@@ -41,12 +44,9 @@ describe('view', () => {
     expect(view.year).toBe(taskAssignment.year)
     expect(view.analystSLA).toBe(taskAssignment.analystSLA)
     expect(view.taskStatus).toBe(taskAssignment.taskStatus)
-    expect(view.analystEmail).toBe(taskAssignment.analystEmail)
-    expect(view.qaEmail).toBe(taskAssignment.qaEmail)
-    expect(view.qaSLA).toBe(taskAssignment.qaSLA)
+    expect(view.analystId).toBe(taskAssignment.analystId)
+    expect(view.qaId).toBe(taskAssignment.qaId)
     expect(view.status).toBe(taskAssignment.status)
-    expect(view.createdAt).toBe(taskAssignment.createdAt)
-    expect(view.updatedAt).toBe(taskAssignment.updatedAt)
     expect(view.createdAt).toBeTruthy()
     expect(view.updatedAt).toBeTruthy()
   })
