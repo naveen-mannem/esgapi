a2d328f956259018e3df617de7fcc9f5f8195709
vivekk99 <vivekkumar1076@gmail.com>
2021-05-10 22:13:53 -0700

    Update controller.js

--- src/api/errorDetails/controller.js
+++ src/api/errorDetails/controller.js
@@ -1,8 +1,8 @@
-import { success, notFound } from '../../services/response/'
+import { success, notFound, authorOrAdmin } from '../../services/response/'
 import { ErrorDetails } from '.'
 
-export const create = ({ bodymen: { body } }, res, next) =>
-  ErrorDetails.create(body)
+export const create = ({ user, bodymen: { body } }, res, next) =>
+  ErrorDetails.create({ ...body, createdBy: user })
     .then((errorDetails) => errorDetails.view(true))
     .then(success(res, 201))
     .catch(next)
@@ -10,6 +10,10 @@ export const create = ({ bodymen: { body } }, res, next) =>
 export const index = ({ querymen: { query, select, cursor } }, res, next) =>
   ErrorDetails.count(query)
     .then(count => ErrorDetails.find(query, select, cursor)
+      .populate('createdBy')
+      .populate('errorTypeId')
+      .populate('taskId')
+      .populate('standaloneId')
       .then((errorDetails) => ({
         count,
         rows: errorDetails.map((errorDetails) => errorDetails.view())
@@ -20,22 +24,32 @@ export const index = ({ querymen: { query, select, cursor } }, res, next) =>
 
 export const show = ({ params }, res, next) =>
   ErrorDetails.findById(params.id)
+    .populate('createdBy')
+    .populate('errorTypeId')
+    .populate('taskId')
+    .populate('standaloneId')
     .then(notFound(res))
     .then((errorDetails) => errorDetails ? errorDetails.view() : null)
     .then(success(res))
     .catch(next)
 
-export const update = ({ bodymen: { body }, params }, res, next) =>
+export const update = ({ user, bodymen: { body }, params }, res, next) =>
   ErrorDetails.findById(params.id)
+    .populate('createdBy')
+    .populate('errorTypeId')
+    .populate('taskId')
+    .populate('standaloneId')
     .then(notFound(res))
+    .then(authorOrAdmin(res, user, 'createdBy'))
     .then((errorDetails) => errorDetails ? Object.assign(errorDetails, body).save() : null)
     .then((errorDetails) => errorDetails ? errorDetails.view(true) : null)
     .then(success(res))
     .catch(next)
 
-export const destroy = ({ params }, res, next) =>
+export const destroy = ({ user, params }, res, next) =>
   ErrorDetails.findById(params.id)
     .then(notFound(res))
+    .then(authorOrAdmin(res, user, 'createdBy'))
     .then((errorDetails) => errorDetails ? errorDetails.remove() : null)
     .then(success(res, 204))
     .catch(next)
