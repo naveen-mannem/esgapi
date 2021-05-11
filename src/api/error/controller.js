4cf88f3687f4bc18985321ff4d599ee26cce5df4
vivekk99 <vivekkumar1076@gmail.com>
2021-05-10 22:13:19 -0700

    Update controller.js

--- src/api/error/controller.js
+++ src/api/error/controller.js
@@ -1,8 +1,8 @@
-import { success, notFound } from '../../services/response/'
+import { success, notFound, authorOrAdmin } from '../../services/response/'
 import { Error } from '.'
 
-export const create = ({ bodymen: { body } }, res, next) =>
-  Error.create(body)
+export const create = ({ user, bodymen: { body } }, res, next) =>
+  Error.create({ ...body, createdBy: user })
     .then((error) => error.view(true))
     .then(success(res, 201))
     .catch(next)
@@ -10,6 +10,7 @@ export const create = ({ bodymen: { body } }, res, next) =>
 export const index = ({ querymen: { query, select, cursor } }, res, next) =>
   Error.count(query)
     .then(count => Error.find(query, select, cursor)
+      .populate('createdBy')
       .then((errors) => ({
         count,
         rows: errors.map((error) => error.view())
@@ -20,22 +21,26 @@ export const index = ({ querymen: { query, select, cursor } }, res, next) =>
 
 export const show = ({ params }, res, next) =>
   Error.findById(params.id)
+    .populate('createdBy')
     .then(notFound(res))
     .then((error) => error ? error.view() : null)
     .then(success(res))
     .catch(next)
 
-export const update = ({ bodymen: { body }, params }, res, next) =>
+export const update = ({ user, bodymen: { body }, params }, res, next) =>
   Error.findById(params.id)
+    .populate('createdBy')
     .then(notFound(res))
+    .then(authorOrAdmin(res, user, 'createdBy'))
     .then((error) => error ? Object.assign(error, body).save() : null)
     .then((error) => error ? error.view(true) : null)
     .then(success(res))
     .catch(next)
 
-export const destroy = ({ params }, res, next) =>
+export const destroy = ({ user, params }, res, next) =>
   Error.findById(params.id)
     .then(notFound(res))
+    .then(authorOrAdmin(res, user, 'createdBy'))
     .then((error) => error ? error.remove() : null)
     .then(success(res, 204))
     .catch(next)
