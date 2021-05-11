52c9a6d1761d70301ddf41e76da9f5e57fe0f363
vivekk99 <vivekkumar1076@gmail.com>
2021-05-10 22:21:50 -0700

    Update index.js

--- src/api/polarityRule/index.js
+++ src/api/polarityRule/index.js
@@ -1,104 +1,99 @@
 import { Router } from 'express'
 import { middleware as query } from 'querymen'
 import { middleware as body } from 'bodymen'
-import { master } from '../../services/passport'
+import { token } from '../../services/passport'
 import { create, index, show, update, destroy } from './controller'
 import { schema } from './model'
 export PolarityRule, { schema } from './model'
 
 const router = new Router()
-const { polarityName, polarityValue, condition, datapointId, status, createdAt, updatedAt } = schema.tree
+const { polarityName, polarityValue, condition, dataPointId, status } = schema.tree
 
 /**
- * @api {post} /polarityRule Create polarity rule
+ * @api {post} /polarityRules Create polarity rule
  * @apiName CreatePolarityRule
  * @apiGroup PolarityRule
- * @apiPermission master
- * @apiParam {String} access_token master access token.
+ * @apiPermission user
+ * @apiParam {String} access_token user access token.
  * @apiParam polarityName Polarity rule's polarityName.
  * @apiParam polarityValue Polarity rule's polarityValue.
  * @apiParam condition Polarity rule's condition.
- * @apiParam datapointId Polarity rule's datapointId.
- * @apiParam status Polarity rule's status.
- * @apiParam createdAt Polarity rule's createdAt.
- * @apiParam updatedAt Polarity rule's updatedAt.
+ * @apiParam dataPointId Polarity rule's dataPointId.
  * @apiSuccess {Object} polarityRule Polarity rule's data.
  * @apiError {Object} 400 Some parameters may contain invalid values.
  * @apiError 404 Polarity rule not found.
- * @apiError 401 master access only.
+ * @apiError 401 user access only.
  */
 router.post('/',
-  master(),
-  body({ polarityName, polarityValue, condition, datapointId, status, createdAt, updatedAt }),
+  token({ required: true }),
+  body({ polarityName, polarityValue, condition, dataPointId }),
   create)
 
 /**
- * @api {get} /polarityRule Retrieve polarity rules
+ * @api {get} /polarityRules Retrieve polarity rules
  * @apiName RetrievePolarityRules
  * @apiGroup PolarityRule
- * @apiPermission master
- * @apiParam {String} access_token master access token.
+ * @apiPermission user
+ * @apiParam {String} access_token user access token.
  * @apiUse listParams
  * @apiSuccess {Number} count Total amount of polarity rules.
  * @apiSuccess {Object[]} rows List of polarity rules.
  * @apiError {Object} 400 Some parameters may contain invalid values.
- * @apiError 401 master access only.
+ * @apiError 401 user access only.
  */
 router.get('/',
-  master(),
+  token({ required: true }),
   query(),
   index)
 
 /**
- * @api {get} /polarityRule/:id Retrieve polarity rule
+ * @api {get} /polarityRules/:id Retrieve polarity rule
  * @apiName RetrievePolarityRule
  * @apiGroup PolarityRule
- * @apiPermission master
- * @apiParam {String} access_token master access token.
+ * @apiPermission user
+ * @apiParam {String} access_token user access token.
  * @apiSuccess {Object} polarityRule Polarity rule's data.
  * @apiError {Object} 400 Some parameters may contain invalid values.
  * @apiError 404 Polarity rule not found.
- * @apiError 401 master access only.
+ * @apiError 401 user access only.
  */
 router.get('/:id',
-  master(),
+  token({ required: true }),
   show)
 
 /**
- * @api {put} /polarityRule/:id Update polarity rule
+ * @api {put} /polarityRules/:id Update polarity rule
  * @apiName UpdatePolarityRule
  * @apiGroup PolarityRule
- * @apiPermission master
- * @apiParam {String} access_token master access token.
+ * @apiPermission user
+ * @apiParam {String} access_token user access token.
  * @apiParam polarityName Polarity rule's polarityName.
  * @apiParam polarityValue Polarity rule's polarityValue.
  * @apiParam condition Polarity rule's condition.
- * @apiParam datapointId Polarity rule's datapointId.
+ * @apiParam dataPointId Polarity rule's dataPointId.
  * @apiParam status Polarity rule's status.
- * @apiParam createdAt Polarity rule's createdAt.
- * @apiParam updatedAt Polarity rule's updatedAt.
  * @apiSuccess {Object} polarityRule Polarity rule's data.
  * @apiError {Object} 400 Some parameters may contain invalid values.
  * @apiError 404 Polarity rule not found.
- * @apiError 401 master access only.
+ * @apiError 401 user access only.
  */
 router.put('/:id',
-  master(),
-  body({ polarityName, polarityValue, condition, datapointId, status, createdAt, updatedAt }),
+  token({ required: true }),
+  body({ polarityName, polarityValue, condition, dataPointId, status }),
   update)
 
 /**
- * @api {delete} /polarityRule/:id Delete polarity rule
+ * @api {delete} /polarityRules/:id Delete polarity rule
  * @apiName DeletePolarityRule
  * @apiGroup PolarityRule
- * @apiPermission master
- * @apiParam {String} access_token master access token.
+ * @apiPermission user
+ * @apiParam {String} access_token user access token.
  * @apiSuccess (Success 204) 204 No Content.
  * @apiError 404 Polarity rule not found.
- * @apiError 401 master access only.
+ * @apiError 401 user access only.
  */
 router.delete('/:id',
-  master(),
+  token({ required: true }),
   destroy)
 
 export default router
