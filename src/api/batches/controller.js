62d8e17127583caff650b67d0cfcb95c0b8770f9
vivekk99 <80243113+vivekk99@users.noreply.github.com>
2021-05-15 01:55:39 -0700

    Update controller.js

--- src/api/batches/controller.js
+++ src/api/batches/controller.js
@@ -11,6 +11,7 @@ export const index = ({ querymen: { query, select, cursor } }, res, next) =>
   Batches.count(query)
     .then(count => Batches.find(query, select, cursor)
       .populate('createdBy')
+      .populate('companyId')
       .then((batches) => ({
         count,
         rows: batches.map((batches) => batches.view())
@@ -22,6 +23,7 @@ export const index = ({ querymen: { query, select, cursor } }, res, next) =>
 export const show = ({ params }, res, next) =>
   Batches.findById(params.id)
     .populate('createdBy')
+    .populate('companyId')
     .then(notFound(res))
     .then((batches) => batches ? batches.view() : null)
     .then(success(res))
@@ -30,6 +32,7 @@ export const show = ({ params }, res, next) =>
 export const update = ({ user, bodymen: { body }, params }, res, next) =>
   Batches.findById(params.id)
     .populate('createdBy')
+    .populate('companyId')
     .then(notFound(res))
     .then(authorOrAdmin(res, user, 'createdBy'))
     .then((batches) => batches ? Object.assign(batches, body).save() : null)
