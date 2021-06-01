66da49636c22f491d605ae1a1dd8e00cb955e5ad
Shivamurugan <shiva.m@indiudmsoft.com>
2021-06-01 21:39:27 +0530

    updated rule methods

--- src/api/derived_datapoints/controller.js
+++ src/api/derived_datapoints/controller.js
@@ -96,8 +96,7 @@ export const calculateForACompany = async ({ user, params }, res, next) => {
         mergedDetails = _.concat(allStandaloneDetails, allBoardMemberMatrixDetails, allKmpMatrixDetails);
 
         // let distinctRuleMethods = await Rules.distinct('methodName').populate('datapointId');
-        let distinctRuleMethods = ["count of"];
-        // ["MatrixPercentage", "Minus", "Sum", "count of", "Ratio", "Percentage", "YesNo", "RatioADD", "As", "ADD", "AsPercentage", "AsRatio", "Condition", "Multiply"];
+        let distinctRuleMethods = ["MatrixPercentage", "Minus", "Sum", "count of", "Ratio", "Percentage", "YesNo", "RatioADD", "As", "ADD", "AsPercentage", "AsRatio", "Condition", "Multiply"];
         //Process all rules
         for (let ruleIndex = 0; ruleIndex < distinctRuleMethods.length; ruleIndex++) {
           switch (distinctRuleMethods[ruleIndex]) {
