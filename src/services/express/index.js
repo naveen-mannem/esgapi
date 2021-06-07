925e37b4e65ba24ceac1eca0bd5b6df6095e45ab
Shivamurugan <shiva.m@indiudmsoft.com>
2021-06-07 21:23:42 +0530

    Removed multer and added body parser again in express/index.js

--- src/services/express/index.js
+++ src/services/express/index.js
@@ -3,7 +3,6 @@ import cors from 'cors'
 import compression from 'compression'
 import morgan from 'morgan'
 import bodyParser from 'body-parser'
-import multer from 'multer';
 import { errorHandler as queryErrorHandler } from 'querymen'
 import { errorHandler as bodyErrorHandler } from 'bodymen'
 import { env } from '../../config'
@@ -18,23 +17,8 @@ export default (apiRoot, routes) => {
     app.use(morgan('dev'))
   }
 
-  const storage = multer.diskStorage({ //multers disk storage settings
-    destination: function(req, file, cb) {
-      // cb(null, './uploads/')
-      cb(null, process.env.PWD + '/uploads');
-    },
-    filename: function(req, file, cb) {
-      var datetimestamp = Date.now();
-      cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length - 1])
-    }
-  });
-
-  const upload = multer({ //multer settings
-    storage: storage,
-    // fileFilter: function(req, file, callback) { //file filter      
-    //     callback(null, true);
-    // }
-  });
+  app.use(bodyParser.urlencoded({ extended: false }))
+  app.use(bodyParser.json())
 
   app.use(apiRoot, routes)
   app.use(queryErrorHandler())
